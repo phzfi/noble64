3988d6beb2e24590f45dc9ebe5dc25ee233d5930
Antti Hätinen <pharazon@phz.fi>
2024-12-26 01:00:47 +0200

    Install held packages

--- scripts/release-upgrade.sh
+++ scripts/release-upgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get -y upgrade
+apt install -y linux-headers-generic linux-headers-virtual linux-image-virtual linux-virtual sosreport
 do-release-upgrade -f DistUpgradeViewNonInteractive
